0177f121661e3aa8f7adaee1e6418322ba532dfb
Gerriss Gailey <gerrissgailey@gmail.com>
2021-01-22 00:30:53 -0700

    Completed all JavaScript functionality. Small addition to style.css. Added README.

---
+++ README.md
@@ -0,0 +1,39 @@
+# JavaScript Code Quiz
+
+This project was designed to serve as a web app where coding students could take a timed quiz and test their knowledge of JavaScript.
+
+This quiz app functions as follows:
+
+- When the page is loaded, the user will have two options. One, to start the quiz. The second, to view previous high scores. 
+
+- When the user presses the "Start" button, a series of multiple choice questions loads and the quiz timer starts it's countdown. 
+
+- A selection of any of the answer buttons will advance to the next question. A selection of a correct answer will add 10 points to the score.
+
+- The user is given 60 seconds to answer a series of five questions related to JavaScript. If the timer reaches zero before the user answers all of the questions, the quiz will end and display a screen where the user can enter their initials to record their score. 
+
+- If the user answers all the quesitons, completing the quiz, then also will the quiz end and display the screen for the user to enter their initials and record their score.
+
+- After submitting the users initials, the High Scores page will be displayed showing all stored users and scores. Local storage is used to store the history of the submitted high scores.
+
+- From the High Scores page, the user can select to either go back to the start page, or to reset the high scores.
+
+
+## Application Preview
+---
+
+The live deployment of this application can be found by [Clicking Here](https://gerrissgailey.github.io/javascript-quiz/).
+
+<br>
+
+## Demo Preview
+---
+
+
+![Password Generator Preview](assets/demo/preview.gif)
+
+<br>
+
+## Authors
+---
+* Gerriss Gailey - JavaScript code
--- script.js
+++ script.js
@@ -1,37 +1,80 @@
+// variables for quiz functionality
 var currentQuestion = 0;
 var score = 0;
 var totalQuestions = questions.length;
 var timer;
-var count = 75;
+var count = 60;
 var displayTime = document.getElementById("display-time")
-
 var startScreenEl = document.getElementById("start-screen");
 var questionsEl = document.getElementById("quiz");
+var currentScore = document.getElementById("current-score")
+var generateBtn = document.getElementById("start");
+var viewScores = document.getElementById("view-scores");
+var endScore = document.getElementById("final-score");
+var submitButton = document.getElementById("submit");
+var backToStart = document.getElementById("back-start");
+var resetScores = document.getElementById("reset-scores");
+
+// variables for question values
 var displayQuestion = document.getElementById("question");
 var ans1 = document.getElementById("answer1");
 var ans2 = document.getElementById("answer2");
 var ans3 = document.getElementById("answer3");
 var ans4 = document.getElementById("answer4");
-var currentScore = document.getElementById("current-score")
-var endScore = document.getElementById("final-score");
-var generateBtn = document.getElementById("start");
 
+// variables for local storage
+var highScores = document.getElementById("high-scores");
+var printInitials = document.getElementById("initials-stored");
+var printScore = document.getElementById("score-stored");
+var recordScore = [];
 
+// reset the timer
 clearInterval(timer);
 
+// initialize from local storage
+init();
+
+// pull data from local storage to display on high scores page
+function renderScores() {
+    // Clear high score elements and update
+    printInitials.innerHTML = "";
+    printScore.innerHTML = "";
+  
+    // Render a new li for each high score
+    for (var i = 0; i < recordScore.length; i++) {
+      var displayScore = recordScore[i];
+        
+      var liInitials = document.createElement("li");
+      liInitials.textContent = displayScore.userInitials;
+      liInitials.setAttribute("data-index", i);
+
+      var liScore = document.createElement("li");
+      liScore.textContent = displayScore.userScore;
+      liScore.setAttribute("data-index", i);
+  
+      // appends high score info to high scores page
+      printInitials.appendChild(liInitials);
+      printScore.appendChild(liScore);
+    }
+}
+
+// display the quiz and start the timer once "start" is clicked
 function setStage() {
     startScreenEl.setAttribute("class", "hide");
     questionsEl.setAttribute("class", "show")
     timer = setInterval(function () {
         displayTime.textContent = count;
         count--;
-        if (count === 0) {
+        if (count < 0) {
+            questionsEl.setAttribute("class", "hide");
+            endScore.setAttribute("class", "show");
             clearInterval(timer);
-            // writeHighScore();
+            return;
         }
     }, 1000)
 };
 
+// cycle through the questions
 function startQuestions(questionIndex) {
     var runQuestions = questions[questionIndex];
     // console.log(runQuestions);
@@ -41,12 +84,9 @@ function startQuestions(questionIndex) {
     ans2.textContent = runQuestions.choice2;
     ans3.textContent = runQuestions.choice3;
     ans4.textContent = runQuestions.choice4;
-    // console.log(ans1);
-    // console.log(ans2);
-    // console.log(ans3);
-    // console.log(ans4);
 };
 
+// listen for which answer button is clicked
 var selectedOption = "";
 let btns = document.querySelectorAll('button.ans');
 
@@ -58,47 +98,107 @@ for (i of btns) {
     });
 }
 
-function loadNextQuestion() {
-    
+// loads the next question in queue
+function loadNextQuestion() {    
     startQuestions(currentQuestion);
 }
 
-
+// updates the score & ends the quiz after last question
 function updateScore() {
-    // console.log(questions[currentQuestion]);
+    // if answer is correct, add 10 points
     if (questions[currentQuestion].answer == selectedOption) {
         score += 10;
         currentScore.textContent = score;
         console.log(score);
-        // alert("Correct! + 10 Points!");
     } else {
-        // if (questions[currentQuestion].answer == !selectedOption) {
-            score -= 5;
-            currentScore.textContent = score;
+        // if answer is wrong, subtract 5 seconds from timer
             count -= 5;
         }
-    if (currentQuestion == totalQuestions - 1) {
-        questionsEl.setAttribute("class", "hide");
-        endScore.setAttribute("class", "show");
-        endScore.textContent = 'Your Score: ' + score;
-        clearInterval(timer);
-        return;
-    }
-    // console.log(questionsEl);
-    // console.log(endScore);
+        // when last question of quiz is answered
+        if (currentQuestion == totalQuestions - 1) {
+            questionsEl.setAttribute("class", "hide");
+            endScore.setAttribute("class", "show");
+            clearInterval(timer);
+            return;
+        }
     currentQuestion++;
     loadNextQuestion();
 }
-
-console.log(score);
-console.log(totalQuestions);
-    // startQuestions(currentQuestion);
     
-    
+// console.log(score);
+// console.log(totalQuestions);
     
 loadNextQuestion();
-// add event listener on start
+    
+// checks local storage for any existing values
+function init() {
+    // Parsing the JSON string to an object
+    var retrieveScore = JSON.parse(localStorage.getItem("userSubmitted"));
+    
+    // if data is found in local storage, update the recordScore array with this data
+    if (retrieveScore !== null) {
+        recordScore = retrieveScore;
+    }
+}
+
+// event listener to store user initials input
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var initialsStored = document.querySelector("#initials").value;
+    var scoreStored = score;
+    
+    // make object from user submission
+    var userSubmitted = {    
+        userInitials: initialsStored,
+        userScore: scoreStored
+    };
+    
+    // push userSubmitted object to recordScore array
+    recordScore.push(userSubmitted);
+    
+    // set new submission to local storage
+    localStorage.setItem("userSubmitted", JSON.stringify(recordScore));
+    highScores.setAttribute("class", "show");
+    endScore.setAttribute("class", "hide");
+    renderScores();
+
+});
+
+// event listener to start the quiz
 generateBtn.addEventListener("click", setStage);
 
+// event listener to view high scores
+viewScores.addEventListener("click", function(event) {
+    event.preventDefault();
 
+    renderScores();
 
+    highScores.setAttribute("class", "show");
+    startScreenEl.setAttribute("class", "hide");
+});
+
+// event listener to return to start screen
+backToStart.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    startScreenEl.setAttribute("class", "show");
+    highScores.setAttribute("class", "hide");
+    displayTime.innerHTML = "";
+    currentScore.innerHTML = "";
+    currentQuestion = 0;
+    count = 60;
+    score = 0;
+    loadNextQuestion();
+});
+
+// event listener to clear scores from local storage
+resetScores.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    printInitials.innerHTML = "";
+    printScore.innerHTML = "";
+    recordScore = [];
+    localStorage.clear();
+});
+    
